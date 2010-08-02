262ef04726b4f34cb2adbb172138b2d5d135c674
Randy Syring <randy.syring@level12.io>
2010-08-02 00:53:05 -0400

    added a CRUD view and related; mainly styled datagrid

--- templatingbwp_ta/application.py
+++ templatingbwp_ta/application.py
@@ -1,13 +1,13 @@
-from os import path
-
 from blazeweb.application import WSGIApp
 from blazeweb.middleware import full_wsgi_stack
 from blazeweb.scripting import application_entry
+from sqlalchemybwp.lib.middleware import SQLAlchemyApp
 
 import templatingbwp_ta.config.settings as settingsmod
 
 def make_wsgi(profile='Dev'):
     app = WSGIApp(settingsmod, profile)
+    app = SQLAlchemyApp(app)
     return full_wsgi_stack(app)
 
 def script_entry():
